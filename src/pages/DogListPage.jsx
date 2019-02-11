e1044797c8b56b04d7f8281c42b64a1a56d28cdd
evgene <evgeniy.panteleev@scorum.com>
2019-02-11 17:38:39 +0300

    rxjs-test

--- src/pages/DogListPage.jsx
+++ src/pages/DogListPage.jsx
@@ -7,21 +7,31 @@ import DogList from '../components/DogList/DogList';
 import ErrorWindow from '../components/ErrorWindow/ErrorWindow';
 
 class DogListPage extends Component {
+    state = {
+      coords:{
+          x: 0,
+          y: 0,
+      }
+    };
   componentDidMount() {
     const{errorModalClose, getAllBreedsJSON}=this.props;
     errorModalClose();
     getAllBreedsJSON();
-    const btn = document.querySelector('.test-btn');
-    const getDataRx = fromEvent(btn, 'click');
 
-    getDataRx.subscribe(e => {console.log(e)});
+    const btn$ = fromEvent(document, 'mousemove');
+
+    btn$.subscribe(e => {this.setState({coords:{
+        x: e.clientX, y: e.clientY
+        }})});
   };
 
   render() {
     const isServerError = this.props.errorModalOpen.value;
     return (
       <Fragment>
+          <h1>X:{this.state.coords.x} Y:{this.state.coords.y}</h1>
         <button className="test-btn">1111</button>
+          <input type="text" className="test-text" />
         {!isServerError
         ? <DogList breedImage={this.props.breedImage}
                    currentBreed={this.props.currentBreed}
