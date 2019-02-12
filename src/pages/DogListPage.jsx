9fe5928c26235f0fa69d071bb7ace9a0ce87ab12
panteleev <evgeniy.panteleev@scorum.com>
2019-02-12 12:53:02 +0300

    try rxjs

--- src/pages/DogListPage.jsx
+++ src/pages/DogListPage.jsx
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { fromEvent, of, Observable} from "rxjs";
+import React, {Component, Fragment} from 'react';
+import {fromEvent, of, Observable, concat } from "rxjs";
 
 import WithViewDogListPage from '../containers/DogListPageWrapper';
 import '../components/DogList/DogList.css';
@@ -8,55 +8,57 @@ import ErrorWindow from '../components/ErrorWindow/ErrorWindow';
 
 class DogListPage extends Component {
     state = {
-      coords:{
-          x: 0,
-          y: 0,
-      }
+        coords: {
+            x: 0,
+            y: 0,
+        }
     };
 
     data$ = Observable.create(this.props.getAllBreedsJSON);
-    btn$ = fromEvent(document, 'mousemove');
+    mouse$ = fromEvent(document, 'mousemove');
     num$ = 5;
-    streams = null;
+    stream$ = concat(this.mouse$, this.data$, this.num$ );
 
-  componentDidMount() {
-    const { errorModalClose } = this.props;
-    errorModalClose();
+    componentDidMount() {
+        const {errorModalClose} = this.props;
+        errorModalClose();
 
-    this.streams = [
-        this.data$.subscribe(),
-        this.btn$.subscribe(e => {this.setState({coords:{
-                x: e.clientX, y: e.clientY
-            }})}),
+        this.data$.subscribe();
+        this.mouse$.subscribe(e => {
+            this.setState({
+                coords: {
+                    x: e.clientX, y: e.clientY
+                }
+            })
+        });
         of(this.num$).subscribe(
             x => console.log(x),
             err => console.log(err),
             () => console.log('complete')
         )
-    ];
-  };
+    };
 
-  componentWillUnmount() {
-      this.streams.map(stream => stream.unsubscribe());
-  }
+    componentWillUnmount() {
+        this.stream$.unsubscribe();
+    }
 
     render() {
-    const isServerError = this.props.errorModalOpen.value;
-    return (
-      <Fragment>
-          <h1>X:{this.state.coords.x} Y:{this.state.coords.y}</h1>
-        <button className="test-btn">1111</button>
-          <input type="text" className="test-text" />
-        {!isServerError
-        ? <DogList breedImage={this.props.breedImage}
-                   currentBreed={this.props.currentBreed}
-                   breeds={this.props.breeds}
-                   setCurrentBreed={this.props.setCurrentBreed}
-                   getBreedImagesJSON={this.props.getBreedImagesJSON} />
-        : <ErrorWindow errorModalClose={this.props.errorModalClose} />}
-      </Fragment>
-    )
-  };
+        const isServerError = this.props.errorModalOpen.value;
+        return (
+            <Fragment>
+                <h1>X:{this.state.coords.x} Y:{this.state.coords.y}</h1>
+                <input type="text"
+                       className="test-text"/>
+                {!isServerError
+                    ? <DogList breedImage={this.props.breedImage}
+                               currentBreed={this.props.currentBreed}
+                               breeds={this.props.breeds}
+                               setCurrentBreed={this.props.setCurrentBreed}
+                               getBreedImagesJSON={this.props.getBreedImagesJSON}/>
+                    : <ErrorWindow errorModalClose={this.props.errorModalClose}/>}
+            </Fragment>
+        )
+    };
 }
 
 export default WithViewDogListPage(DogListPage);
